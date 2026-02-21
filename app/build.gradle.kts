abff8a0941092f78fc629e72cfba4f68da254817
Aabir <ex-aabir.daneshyar@optimusinfo.com>
2026-02-21 15:38:53 +0530

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.checklistdev.tasks"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.11"
+        versionCode = 12
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
