3f88e744b41702a7b0ceb82d582abea593a93071
Aabir <ex-aabir.daneshyar@optimusinfo.com>
2026-02-27 17:57:33 +0530

    Version update to 1.0.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.checklistdev.tasks"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.13"
+        versionCode = 14
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
