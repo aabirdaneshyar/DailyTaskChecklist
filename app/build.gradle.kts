849d1925c2e14768f1f7ed8d5d8200561c6d8d4c
Aabir <ex-aabir.daneshyar@optimusinfo.com>
2026-03-12 16:05:37 +0530

    Signup Release version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.checklistdev.tasks"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.15"
+        versionCode = 16
+        versionName = "1.0.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
